0166367f8d4e302f6b5255f977b0bc1bc810562e
Joel Sequeira <joensg@gmail.com>
2020-09-13 17:25:31 +0100

    change to use vue 2

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
+<div id="app">
   <img alt="Vue logo" src="./assets/logo.png">
   <Master msg="Family Tree App"/>
+</div>
 </template>
 
 <script>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,13 @@
-import { createApp } from 'vue'
+import Vue from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import VueLodash from 'vue-lodash'
+import lodash from 'lodash'
+ 
+// name is optional
+Vue.use(VueLodash, { name: 'custom' , lodash: lodash })
+
+new Vue({
+  el: '#app',
+  render: (h) => h(App)
+})
