45dd33b7545b381e2bad9f802e1e77d397c96fdc
Joel Sequeira <joensg@gmail.com>
2020-09-13 18:07:22 +0100

    wip

--- src/components/Master.vue
+++ src/components/Master.vue
@@ -1,20 +1,111 @@
 <template>
   <div class="master">
     <h1>{{ msg }}</h1>
+    <div id="graph"></div>
   </div>
 </template>
 
 <script>
+// eslint-disable-next-line no-unused-vars
+const d3 = require("d3");
+window.d3 = d3;
+import dTree from 'd3-dtree'
+
 export default {
   name: 'Master',
   props: {
     msg: String
+  },
+  created() {
+    var treeData = [{
+      "name": "Niclas Superlongsurname",
+      "class": "man",
+      "textClass": "emphasis",
+      "marriages": [{
+        "spouse": {
+          "name": "Iliana",
+          "class": "woman",
+          "extra": {
+            "nickname": "Illi"
+          }
+        },
+        "children": [{
+          "name": "James",
+          "class": "man",
+          "marriages": [{
+            "spouse": {
+              "name": "Alexandra",
+              "class": "woman"
+            },
+            "children": [{
+              "name": "Eric",
+              "class": "man",
+              "marriages": [{
+                "spouse": {
+                  "name": "Eva",
+                  "class": "woman"
+                }
+              }]
+            }, {
+              "name": "Jane",
+              "class": "woman"
+            }, {
+              "name": "Jasper",
+              "class": "man"
+            }, {
+              "name": "Emma",
+              "class": "woman"
+            }, {
+              "name": "Julia",
+              "class": "woman"
+            }, {
+              "name": "Jessica",
+              "class": "woman"
+            }]
+          }]
+        }]
+      }]
+    }];
+
+    dTree.init(treeData, {
+      target: "#graph",
+      debug: true,
+      height: 800,
+      width: 1200,
+      callbacks: {
+        nodeClick: function(name, extra) {
+          console.log(name);
+          console.log(extra);
+        },
+        textRenderer: function(name, extra, textClass) {
+          // THis callback is optinal but can be used to customize
+          // how the text is rendered without having to rewrite the entire node
+          // from screatch.
+          if (extra && extra.nickname)
+            name = name + " (" + extra.nickname + ")";
+          return "<p align='center' class='" + textClass + "'>" + name + "</p>";
+        },
+        nodeRenderer: function(name, x, y, height, width, extra, id, nodeClass, textClass, textRenderer) {
+          // This callback is optional but can be used to customize the
+          // node element using HTML.
+          let node = '';
+          node += '<div ';
+          node += 'style="height:100%;width:100%;" ';
+          node += 'class="' + nodeClass + '" ';
+          node += 'id="node' + id + '">\n';
+          node += textRenderer(name, extra, textClass);
+          node += '</div>';
+          return node;
+      }
+    }
+  });
+
   }
 }
 </script>
 
 
-<style scoped>
+<style>
 h3 {
   margin: 40px 0 0;
 }
@@ -28,5 +119,40 @@ li {
 }
 a {
   color: #42b983;
+}
+
+body {
+    font: 10px sans-serif;
+}
+.linage {
+    fill: none;
+    stroke: #000;
+}
+.marriage {
+    fill: none;
+    stroke: black;
+}
+.man {
+    background-color: lightblue;
+    border-style: solid;
+    border-width: 1px;
+    box-sizing: border-box;
+}
+.woman {
+    background-color: pink;
+    border-style: solid;
+    border-width: 1px;
+    box-sizing: border-box;
+}
+.emphasis{
+    font-style: italic;
+}
+p {
+  padding:0;
+  margin:0;
+}
+svg {
+  border-style: solid;
+  border-width: 1px;
 }
 </style>
